03470e452093f5be30d6d694b85adbf6bb6aa502
lag1101 <lomanovasiliy@gmail.com>
2015-02-18 17:27:29 +0400

    changed title to settings value

--- routes/frontpage.js
+++ routes/frontpage.js
@@ -3,10 +3,11 @@
  */
 var express = require('express');
 var router = express.Router();
+var config = require('config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('frontpage', { title: 'Express' });
+    res.render('frontpage', { title: config.get('title') });
 });
 
 module.exports = router;
