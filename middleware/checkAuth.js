0cb9d5bbd8d104ea43f95edf13c9b86adebbafe1
lag1101 <lomanovasiliy@gmail.com>
2015-02-19 16:30:20 +0400

    fixed checking of user session, earlier zeros user couldn't pass it

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -5,7 +5,7 @@
 var HttpError = require('../error').HttpError;
 
 module.exports = function(req, res, next) {
-    if (!req.session.user) {
+    if (req.session.user === undefined) {
         return next(new HttpError(401, "Вы не авторизованы"));
     }
 
