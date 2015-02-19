a7e58bc199537a42c1285eabcf1ca7584d7103e7
lag1101 <lomanovasiliy@gmail.com>
2015-02-19 15:47:02 +0400

    fixed heroku failing

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ var checkAuth = require('../middleware/checkAuth');
 
 module.exports = function(app) {
 
-    app.use('/', require('frontpage'));
+    app.use('/', require('./frontpage'));
 
-    app.use('/login', require('login'));
-    app.use('/logout',checkAuth, require('logout'));
+    app.use('/login', require('./login'));
+    app.use('/logout',checkAuth, require('./logout'));
 
-    app.use('/game', checkAuth, require('game'));
+    app.use('/game', checkAuth, require('./game'));
 };
