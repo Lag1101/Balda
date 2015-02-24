91bc6d0cc585e16a02d9f85a18d2cb5944d15687
lag1101 <lomanovasiliy@gmail.com>
2015-02-24 17:15:44 +0400

    we'll use this structure of sockets, until we find better

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ var logger = require('morgan');
 var CookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var AuthError = require('./models/UserModel').AuthError;
 var HttpError = require('./models/UserModel').HttpError;
-var users = require('./models/UserModel').users;
-var debug = require('debug')('Balda:server');
 var http = require('http');
 var config = require('./config');
-var async = require('async');
+var AuthError = require('./models/UserModel').AuthError;
 
 
 var cookieParser = CookieParser;
@@ -71,96 +68,9 @@ app.use(function(err, req, res, next) {
 
 var server = http.createServer(app);
 server.listen(config.get('PORT'), function(){
-    debug('Listening on ' + config.get('PORT'));
+    console.log('Listening on ' + config.get('PORT'));
 });
 
-var io = require('socket.io').listen(server);
-
-var SessionSockets = require('session.socket.io-express4')
-    , sessionSockets = new SessionSockets(io, sessionStore, cookieParser());
-
-
-sessionSockets.on('connection', function (err, socket, session) {
-
-    function loadSession(sid, callback) {
-        sessionStore.get(sid[config.get('session:key')], function(err, session) {
-            if(err) {
-                return callback(err, null);
-            }
-            if (arguments.length == 0 || !session) {
-                // no arguments => no session
-                return callback(new Error("Cannot load session"), null);
-            } else {
-                return callback(null, session);
-            }
-        });
-    }
-    function loadUser(session, callback) {
-
-        if (session.user === undefined) {
-            console.log("Session %s is anonymous", session.id);
-            return callback(null, null);
-        }
-
-        console.log("retrieving user ", session.user);
-
-        callback(null, users.get(session.user));
-
-    }
-    function loadUserBySid(sid, callback) {
-        async.waterfall([
-                function(callback) {
-                    loadSession(sid, callback);
-                },
-                function(session, callback) {
-
-                    if (!session) {
-                        return callback(new HttpError(401, "No session"));
-                    }
-
-                    socket.handshake.session = session;
-
-                    loadUser(session, callback)
-                },
-                function(user, callback) {
-                    if (!user) {
-                        callback(new HttpError(403, "Anonymous session may not connect"));
-                    }
-
-                    socket.handshake.user = user;
-
-                    callback(null);
-                }
-
-            ],
-            function(err){
-                callback(err, socket);
-            });
-    }
-    function fullLoadUser (socket, callback) {
-        var cookies = socket.handshake.cookies;
-        var sid = cookieParser.signedCookies(cookies, config.get('session:secret'));
-        loadUserBySid(sid, callback);
-    }
-
-    fullLoadUser(socket, function(err, socket){
-        if(err)
-            return console.error(err);
-
-        var user = socket.handshake.user;
-
-        console.log(user.username + ' connected');
-
-        socket.on('message', function(text, cb) {
-            socket.broadcast.emit('message', user.username, text);
-            cb && cb();
-        });
-        socket.on('disconnect', function () {
-
-            console.log(user.username + ' disconnected');
-        });
-    });
-
-});
+require('./socket')(server, sessionStore, cookieParser);
 
 module.exports = app;
