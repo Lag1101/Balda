f002fd00aac605bf624c6df1d8493e4e8f3d508e
lag1101 <lomanovasiliy@gmail.com>
2015-04-08 11:29:28 +0400

    created utils lib Queue moved there

--- game/index.js
+++ game/index.js
@@ -2,42 +2,9 @@
  * Created by vasiliy.lomanov on 06.04.2015.
  */
 
+var Queue = require('../lib/Utils').Queue;
+
 var _id = 0;
-
-function Queue(){
-    this.length = 0;
-    this.elements = {};
-    this.keys = [];
-}
-
-Queue.prototype.push = function(key, val){
-    if(val === undefined) return null;
-
-    var sKey = key.toString();
-
-    this.keys.push(sKey);
-    this.elements[sKey] = val;
-};
-Queue.prototype.erase = function(key){
-    var sKey = key.toString();
-    if(this.exist(sKey)){
-        this.keys.splice(this.keys.indexOf(sKey), 1);
-        this.elements[sKey] = undefined;
-    }
-
-};
-Queue.prototype.len = function(){
-    return this.keys.length;
-};
-Queue.prototype.get = function(key){
-    var sKey = key.toString();
-    return this.elements[sKey];
-};
-Queue.prototype.exist = function(key){
-    var sKey = key.toString();
-    return this.elements[sKey] !== undefined;
-};
-
 function Game() {
     this._id = _id;
     _id ++;
