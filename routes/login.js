a54c5770ebea00b8a4c6a79ce94eb25f7d86f530
lag1101 <lomanovasiliy@gmail.com>
2015-04-13 14:15:49 +0400

    fixed login in

--- routes/login.js
+++ routes/login.js
@@ -17,13 +17,7 @@ router.route('/')
         var password = req.body.password;
 
         User.authorize(username, password, function(err, user) {
-            if (err) {
-                if (err instanceof AuthError) {
-                    return next(new HttpError(403, err.message));
-                } else {
-                    return next(err);
-                }
-            }
+            if (err) return next(err);
 
             req.session.user = user._id;
             res.end();
